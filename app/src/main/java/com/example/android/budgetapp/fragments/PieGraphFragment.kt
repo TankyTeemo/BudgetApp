24bd94f860ba0437ee3149906e51443eac508bd3
Joseph Peroutky <peroutkyj1008@my.uwstout.edu>
2019-11-12 10:51:49 -0600

    added blank files to the folders

---
+++ app/src/main/java/com/example/android/budgetapp/fragments/PieGraphFragment.kt
@@ -0,0 +1,26 @@
+package com.example.android.budgetapp.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.android.budgetapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PieGraphFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pie_graph, container, false)
+    }
+
+
+}
