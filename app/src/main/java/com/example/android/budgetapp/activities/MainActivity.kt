9423ed1a895ee4aeffb40b6b8dcc72246235164e
TankyTeemo <6shwnm@gmail.com>
2019-11-20 17:15:21 -0600

    created calendar screen

--- app/src/main/java/com/example/android/budgetapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/android/budgetapp/activities/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.android.budgetapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.budgetapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 //
+    /**
+     * Called when app is launched
+     * @param savedInstanceState used if orientation is changed otherwise null
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
