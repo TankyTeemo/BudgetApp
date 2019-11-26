be3fb0c8b27fe4d60c3df88ee5c38f8618045e5f
Jake Anderson <andersonj9705@my.uwstout.edu>
2019-11-26 10:04:29 -0600

    pie graph fragment

--- app/src/main/java/com/example/android/budgetapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/android/budgetapp/activities/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.android.budgetapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.android.budgetapp.R
+import com.example.android.budgetapp.fragments.PieGraphFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_pie_graph.*
 
 class MainActivity : AppCompatActivity() {
-//
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        graph_button.setOnClickListener{
+            val fragment: Fragment = PieGraphFragment.newInstance()
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.container, fragment).commit()
+        }
     }
+
+
 }
