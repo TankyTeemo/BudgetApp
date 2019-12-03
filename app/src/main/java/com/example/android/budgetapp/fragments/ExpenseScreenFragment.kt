52fde4dac76c065588fcf12a3f480e26aa459843
TankyTeemo <6shwnm@gmail.com>
2019-12-03 10:12:39 -0600

    Fixed nav up buttons

--- app/src/main/java/com/example/android/budgetapp/fragments/ExpenseScreenFragment.kt
+++ app/src/main/java/com/example/android/budgetapp/fragments/ExpenseScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.example.android.budgetapp.fragments
 
+import android.app.Application
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -7,18 +8,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.android.budgetapp.R
+import com.example.android.budgetapp.database.BudgetDatabase
+import com.example.android.budgetapp.database.BudgetRepository
+import com.example.android.budgetapp.database.daos.CategoryDao
+import com.example.android.budgetapp.database.entities.Category
 import com.example.android.budgetapp.databinding.FragmentExpenseScreenBinding
 
 
 class ExpenseScreenFragment : Fragment() {
 
+    private var databaseHelper: BudgetRepository? = null
+    private var categories: TextView? = null
+    private var arrayList: ArrayList<String>? = null
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         val binding: FragmentExpenseScreenBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_expense_screen, container, false)
+
+        //databaseHelper = BudgetRepository()
+
+        //var arraylist = databaseHelper!!.categoryDao.getAllCategories()
+
         binding.backButton.setOnClickListener { v: View ->
             v.findNavController().navigate(R.id.action_expenseScreenFragment_to_calendarScreenFragment)
         }
