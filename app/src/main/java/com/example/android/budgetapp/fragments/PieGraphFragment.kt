4d6aff904662dc9f362db173966f6233cebd16c2
Jake Anderson <andersonj9705@my.uwstout.edu>
2019-12-12 23:01:19 -0600

    Color added and frame for database implementation

--- app/src/main/java/com/example/android/budgetapp/fragments/PieGraphFragment.kt
+++ app/src/main/java/com/example/android/budgetapp/fragments/PieGraphFragment.kt
@@ -1,6 +1,7 @@
 package com.example.android.budgetapp.fragments
 
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -11,6 +12,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_pie_graph.*
 import com.example.android.budgetapp.R
+import com.example.android.budgetapp.database.BudgetRepository
+import com.example.android.budgetapp.database.BudgetViewModel
+import com.example.android.budgetapp.database.entities.Budget
 import com.example.android.budgetapp.databinding.FragmentCalendarScreenBinding
 import com.example.android.budgetapp.databinding.FragmentPieGraphBinding
 import com.github.mikephil.charting.charts.PieChart
@@ -18,24 +22,27 @@ import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.ColorTemplate.*
+import java.lang.NullPointerException
+import kotlin.collections.ArrayList
 
 /**
  * A simple [Fragment] subclass.
  */
 class PieGraphFragment : Fragment() {
 
-    val expenditureString:Array<String> = arrayOf("Food", "Rent", "Other")
-    val floatValues = arrayOf(22.2f, 50.2f, 12.0f)
-
+    val expenditureString:Array<String> = arrayOf("NONE", "NONE", "NONE")
+    val floatValues = arrayOf(1.0f, 1.0f, 1.0f)
+    private lateinit var binding : FragmentPieGraphBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val binding: FragmentPieGraphBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_pie_graph, container, false)
 
-        setupPieChart(binding, expenditureString)
+        setupPieChart(expenditureString)
 
         binding.backButton.setOnClickListener { v: View ->
             v.findNavController().navigate((R.id.action_pieGraphFragment_to_calendarScreenFragment))
@@ -44,35 +51,60 @@ class PieGraphFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        setupPieChart(expenditureString)
+        Log.i("tags", "onStart")
+        super.onStart()
+
+    }
+
     /**
      * function to setup up the pie chart with the data
      */
-    private fun setupPieChart(binding: FragmentPieGraphBinding, entries :Array<String>) {
+    private fun setupPieChart(entries :Array<String>) {
+        val repo = BudgetRepository(activity!!.application)
+        val budgetList = repo.getBudgets()
+        val categoryList = repo.getCategories()
 
         val pieEntries = ArrayList<PieEntry>(0)
 
-        for (i in 0 until floatValues.size){
-            val myEntry = PieEntry(floatValues[i], entries[i])
+        if(budgetList.value?.size != null){
+            Log.i("tags", budgetList.value?.size.toString())
+            for (i in 0 until budgetList.value!!.size) {
+                val entryFloat = budgetList.value?.get(i)?.amount
+                val categoryString = categoryList.value?.get(i)?.title
+                val myEntry = PieEntry(entryFloat!!.toFloat(), categoryString)
 
-            pieEntries.add(myEntry)
+                pieEntries.add(myEntry)
 
-            Log.i("tags", pieEntries.toString())
+                Log.i("tags", pieEntries.toString())
+            }
         }
+        else{ // Default PieChart when no data is entered
+            for (i in 0 until floatValues.size){
+                val myEntry = PieEntry(floatValues[i], entries[i])
 
+                pieEntries.add(myEntry)
+
+                Log.i("tags", pieEntries.toString())
+            }
+        }
         val dataSet = PieDataSet(pieEntries, "Percentage of Expenditures")
+        dataSet.setColors(JOYFUL_COLORS, 100)
+
         val myData = PieData(dataSet)
         myData.setValueFormatter(PercentFormatter())
-
 
         binding.pieChart.data = myData
         binding.pieChart.description.text = "HI"
         binding.pieChart.isDrawHoleEnabled= false
         binding.pieChart.setUsePercentValues(true)
+        binding.pieChart.setEntryLabelColor(Color.BLACK)
+        binding.pieChart.animateY(1000)
         // pieChart.setOnChartValueSelectedListener(this)
-
         binding.pieChart.invalidate()
-
     }
+
     companion object {
         fun newInstance(): PieGraphFragment = PieGraphFragment()
     }
